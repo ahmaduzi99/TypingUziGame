3b8e8da9e0a198fbe5b6103211864f59091bcdfe
fauzi <ahmaduziagustus2001@gmail.com>
2023-01-15 22:20:49 +0700

    Add project files.

---
+++ TypingUziGame/Models/Stats.cs
@@ -0,0 +1,21 @@
+﻿namespace TypingUziGame.Models
+{
+    public class Stats
+    {
+      
+        public int Correct { get; set; }
+        public int Missed { get; set; }
+        public int Total { get;  set; }
+        public int Accuracy { get; set; }
+
+        public void countTotal()
+        {
+            Total = Correct + Missed;
+        }
+
+        public void CountAccuracy()
+        {
+            Accuracy = Correct * 100 / (Correct + Missed);
+        }
+    }
+}
